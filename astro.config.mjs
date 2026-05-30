4dbe342a8f137d0c7fdb5b5082334ace689da49f
ganidande905 <ganidande9@gmail.com>
2026-05-31 04:16:24 +0530

    feat: UI remap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   vite: {
     plugins: [tailwind()],
   },
+
+  integrations: [react()],
 });
